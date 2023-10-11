a3da94b61c6d0192445115aa80eebe497c3acf62
Vigil1Light <zerry319.wit@gmail.com>
2023-10-11 16:30:26 +0800

    load for save for later

--- MSDHSystem/MSDHSystem/ViewModels/TimeStudyFormsViewModel.cs
+++ MSDHSystem/MSDHSystem/ViewModels/TimeStudyFormsViewModel.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Data.SqlClient;
+using System.Globalization;
 using System.Linq;
 using Xamarin.Forms;
 
@@ -13,6 +14,8 @@ namespace MSDHSystem.ViewModels
     public class TimeStudyFormsViewModel : BaseViewModel, INotifyPropertyChanged
     {
         private bool isloading = false;
+        private bool ischecked;
+        private bool isenabled = true;
         private string name;
         private string classification;
         private string orgcode;
@@ -22,10 +25,23 @@ namespace MSDHSystem.ViewModels
         private string endDate;
         private string supervisoremail;
 
+        private TimeStudyDate timeStudyDate = new TimeStudyDate();
         public bool IsLoading
         {
             get => isloading;
             set => SetProperty(ref isloading, value);
+        }
+
+        public bool IsChecked
+        {
+            get => ischecked;
+            set => SetProperty(ref ischecked, value);
+        }
+
+        public bool IsEnabled
+        {
+            get => isenabled;
+            set => SetProperty(ref isenabled, value);
         }
 
         public string Name
@@ -104,7 +120,7 @@ namespace MSDHSystem.ViewModels
             TimeStudyItems = new ObservableCollection<TimeStudyData>();
             if (Application.Current.Properties.ContainsKey("TimeStudyDateValue"))
             {
-                TimeStudyDate timeStudyDate = (TimeStudyDate)Application.Current.Properties["TimeStudyDateValue"];
+                timeStudyDate = (TimeStudyDate)Application.Current.Properties["TimeStudyDateValue"];
                 GetValue(timeStudyDate);
                 AppSessionManager.Instance.StartSession();
             }
@@ -123,18 +139,34 @@ namespace MSDHSystem.ViewModels
         {
             int tHours = TimeStudyItems.Sum(item => Convert.ToInt32(item.TotalHours));
             int tMins = TimeStudyItems.Sum(item => Convert.ToInt32(item.TotalMins));
-            Hours = (tHours+ (tMins / 60)).ToString();
+            Hours = (tHours + (tMins / 60)).ToString();
             Mins = (tMins % 60).ToString();
         }
 
         private void OnSubmitForReviewClicked(object obj)
         {
-            
+            if (!ischecked || SupervisorEmail == "")
+            {
+                DependencyService.Get<Toast>().Show("Please sign and input supervisor email");
+            }
         }
 
         private void OnSaveForLaterClicked(object obj)
         {
-            DependencyService.Get<Toast>().Show(TimeStudyItems[0].M1.ToString());
+            int n = 0;
+            for(int i = 0; i < TimeStudyItems.Count; i++)
+            {
+                if (timeStudyItems[i].Program == null) break;
+                n++;
+            }
+            if(n == 0)
+            {
+                DependencyService.Get<Toast>().Show("Please input some data, can't save now");
+            }
+            else
+            {
+
+            }
         }
 
         private string hours;
@@ -214,7 +246,78 @@ namespace MSDHSystem.ViewModels
                 }
                 reader.Close();
 
-                for(int i = 0; i < 19; i++)
+                int n = 0;
+                if (timeStudyDate.status == "(Saved for Later)")
+                {
+                    List<int> Date = timeStudyDate.startDate.ToString().Split('/').Select(int.Parse).ToList();
+                    DateTime startDate = new DateTime(Date[2], Date[0], Date[1]);
+                    strQuery = string.Format("SELECT * FROM TimeStudyDetail AS a INNER JOIN TimeStudyProgram AS b ON a.Program = b.ProgramCode INNER JOIN TimeStudyActivity AS c ON CAST(a.Activity AS INTEGER) = c.ActivityNumber WHERE pid_nmbr = '{0}' AND CalenderYear = '{1}' AND CalenderWeek = '{2}'", Xamarin.Essentials.SecureStorage.GetAsync("pid_number").Result, startDate.Year.ToString(), GetWeekNumber(startDate).ToString());
+                    command = new SqlCommand(strQuery, con);
+                    reader = command.ExecuteReader();
+                    
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            n++;
+                            List<string> tHour = new List<string>();
+                            List<string> tMin = new List<string>();
+                            for (int i = 3; i < 8; i++)
+                            {
+                                if (reader[i] == null || reader[i].ToString() == "")
+                                {
+                                    tHour.Add("");
+                                    tMin.Add("");
+                                }
+                                else
+                                {
+                                    List<int> time = reader[i].ToString().Split(':').Select(int.Parse).ToList();
+                                    tHour.Add(time[0].ToString());
+                                    tMin.Add(time[1].ToString());
+                                }  
+                            }
+                            for (int i = 18; i < 20; i++)
+                            {
+                                if (reader[i] == null || reader[i].ToString() == "")
+                                {
+                                    tHour.Add("");
+                                    tMin.Add("");
+                                }
+                                else
+                                {
+                                    List<int> time = reader[i].ToString().Split(':').Select(int.Parse).ToList();
+                                    tHour.Add(time[0].ToString());
+                                    tMin.Add(time[1].ToString());
+                                }
+                            }
+                            TimeStudyItems.Add(new TimeStudyData
+                            {
+                                No = n,
+                                Programs = programs,
+                                Activities = activities,
+                                Program = reader["ProgramCode"].ToString() + " " + reader["ProgramName"].ToString(),
+                                Activity = reader["ActivityNumber"].ToString() + " " + reader["ActivityName"].ToString(),
+                                H1 = tHour[0],
+                                H2 = tHour[1],
+                                H3 = tHour[2],
+                                H4 = tHour[3],
+                                H5 = tHour[4],
+                                H6 = tHour[5],
+                                H7 = tHour[6],
+                                M1 = tMin[0],
+                                M2 = tMin[1],
+                                M3 = tMin[2],
+                                M4 = tMin[3],
+                                M5 = tMin[4],
+                                M6 = tMin[5],
+                                M7 = tMin[6],
+                            });
+                        }
+                    }
+                    reader.Close();
+                }
+
+                for (int i = n; i < 19; i++)
                 {
                     TimeStudyItems.Add(new TimeStudyData
                     {
@@ -231,6 +334,11 @@ namespace MSDHSystem.ViewModels
             {
                 DependencyService.Get<Toast>().Show(ex.Message);
             }
+        }
+
+        public static int GetWeekNumber(DateTime thisDate)
+        {
+            return CultureInfo.InvariantCulture.Calendar.GetWeekOfYear(thisDate, System.Globalization.CalendarWeekRule.FirstDay, DayOfWeek.Sunday);
         }
 
     }
--- MSDHSystem/MSDHSystem/Views/TimeStudyPage.xaml.cs
+++ MSDHSystem/MSDHSystem/Views/TimeStudyPage.xaml.cs
@@ -3,6 +3,7 @@ using MSDHSystem.Utils;
 using MSDHSystem.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading;
