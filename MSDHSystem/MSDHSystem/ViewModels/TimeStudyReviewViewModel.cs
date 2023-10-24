f350d277a863bddd968b3e44a03045f1d9a7ba2e
Vigil1Light <zerry319.wit@gmail.com>
2023-10-24 11:15:40 +0800

    fix calculate date from week number

--- MSDHSystem/MSDHSystem/ViewModels/TimeStudyReviewViewModel.cs
+++ MSDHSystem/MSDHSystem/ViewModels/TimeStudyReviewViewModel.cs
@@ -172,11 +172,11 @@ namespace MSDHSystem.ViewModels
             var weekNum = weeknumber;
             // As we're adding days to a date in Week 1,
             // we need to subtract 1 in order to get the right date for week #1
-            if (firstWeek == 1)
+            /*if (firstWeek == 1)
             {
-                weekNum -= 1;
-            }
-
+                
+            }*/
+            weekNum -= 1;
             // Using the first Thursday as starting week ensures that we are starting in the right year
             // then we add number of weeks multiplied with days
             var result = firstThursday.AddDays(weekNum * 7);
