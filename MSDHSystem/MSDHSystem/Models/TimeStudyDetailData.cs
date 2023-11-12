4e83fe3e6a395881021737b3046a6eeec569878a
Vigil1Light <zerry319.wit@gmail.com>
2023-11-12 13:51:42 +0800

    complete page and model

--- MSDHSystem/MSDHSystem/Models/TimeStudyDetailData.cs
+++ MSDHSystem/MSDHSystem/Models/TimeStudyDetailData.cs
@@ -1,10 +1,37 @@
-﻿using System;
+﻿using MSDHSystem.Utils;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace MSDHSystem.Models
 {
-    class TimeStudyDetailData
+    public class TimeStudyDetailData
     {
+        public int No { get; set; }
+        public Color BackColor { get; set; }
+
+        private string H1;
+        private string H2;
+        private string H3;
+        private string H4;
+        private string H5;
+        private string H6;
+        private string H7;
+        private string M1;
+        private string M2;
+        private string M3;
+        private string M4;
+        private string M5;
+        private string M6;
+        private string M7;
+        private string TotalHours { get; set; }
+        private string TotalMins { get; set; }
+        private string Program { get; set; }
+        private string Activity { get; set; }
+
     }
 }
