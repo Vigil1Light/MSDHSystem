73cb9f25dd437cc6f80a118fe72aee649d972437
Zerry Wit <zerry319.wit@gmail.com>
2024-01-30 11:09:09 +0800

    add lab time study submitted page

--- MSDHSystem/MSDHSystem/Views/LabTimeStudyApprovePage.xaml.cs
+++ MSDHSystem/MSDHSystem/Views/LabTimeStudyApprovePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MSDHSystem.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,25 @@ namespace MSDHSystem.Views
         public LabTimeStudyApprovePage()
         {
             InitializeComponent();
+            this.BindingContext = new LabTimeStudyApproveViewModel();
+        }
+        private async void lstTimeStudyApprove_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            /*try
+            {
+                var selectedProduct = (TimeStudyApproveData)e.Item;
+                Application.Current.Properties["TimeStudyDetailValue"] = selectedProduct;
+
+                DependencyService.Get<Toast>().Show(selectedProduct.Name);
+                IsLoading(true);
+                Thread newThread = new Thread(new ParameterizedThreadStart(LongRunningTask));
+                newThread.Start();
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message.ToString(), "Ok");
+            }
+            lstTimeStudyApprove.SelectedItem = null;*/
         }
     }
 }
